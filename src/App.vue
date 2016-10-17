31011cf97c96dbe465c8036984a62809f24b871d
hfercc <137133744@qq.com>
2016-10-17 22:35:28 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <vux></vux>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Vux from './components/Vux'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,Vux
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Vux.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <group>
+      <cell title="vue" value="cool"></cell>
+    </group>
+  </div>
+</template>
+
+<script>
+  import Group from 'vux/src/components/group'
+  import Cell from 'vux/src/components/cell'
+
+  export default {
+    components: {
+      Group,
+      Cell
+    }
+  }
+</script>
